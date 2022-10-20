6b1cc7ac8af877961d224425b17ca758c5dd219f
Skyler <ho971122@gmail.com>
2022-10-20 20:27:01 +0900

    Update 10808_알파벳 개수.swift

--- Skyler/10808_알파벳 개수.swift
+++ Skyler/10808_알파벳 개수.swift
@@ -1,11 +1,10 @@
 import Foundation
 
-let input = readLine()
+var array = [Int](repeating: 0, count: 26)
+let line = readLine()!
 
-var char = [Int](Array(repeating: 0, count: 26))
-
-input?.forEach {
-    char[Int($0.asciiValue!)-97] += 1
+for ch in line {
+    array[Int(ch.asciiValue!)-97] += 1
 }
 
-print(char)
+print(array.map{ "\($0)" }.joined(separator: " "))
